9156a51d23646d0e84f55bc3f4b7a867cf9c2f28
Flatcova <m.covar20@gmail.com>
2016-09-12 22:44:14 -0500

    testing

--- config/assets/production.js
+++ config/assets/production.js
@@ -21,5 +21,16 @@ module.exports = {
     },
     css: 'public/dist/application.min.css',
     js: 'public/dist/application.min.js'
+  },
+  server: {
+    gruntConfig: 'gruntfile.js',
+    gulpConfig: 'gulpfile.js',
+    allJS: ['server.js', 'config/**/*.js', 'modules/*/server/**/*.js'],
+    models: 'modules/*/server/models/**/*.js',
+    routes: ['modules/!(core)/server/routes/**/*.js', 'modules/core/server/routes/**/*.js'],
+    sockets: 'modules/*/server/sockets/**/*.js',
+    config: 'modules/*/server/config/*.js',
+    policies: 'modules/*/server/policies/*.js',
+    views: 'modules/*/server/views/*.html'
   }
 };
