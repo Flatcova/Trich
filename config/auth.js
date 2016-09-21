caf222def9d383cb498bb45517b21b5aace560e6
Flatcova <m.covar20@gmail.com>
2016-09-21 00:53:15 -0500

    fix callbacks

--- config/auth.js
+++ config/auth.js
@@ -4,19 +4,19 @@ module.exports = {
     'facebookAuth' : {
         'clientID'      : '122795888178850', // your App ID
         'clientSecret'  : '06b3dee149a8eda263a881151e932dbf', // your App Secret
-        'callbackURL'   : 'http://localhost:3000/auth/facebook/callback'
+        'callbackURL'   : 'http://http://trych-env.us-east-1.elasticbeanstalk.com/auth/facebook/callback'
     },
 
     'twitterAuth' : {
         'consumerKey'       : 'L1LZASfV55TdcKgp5PiXHQvt8',
         'consumerSecret'    : 'ObQVZ7jlVz8cHiV2W3nELaQZhAgAr1Q2mU8AJ98W2ZZGEVwvcb',
-        'callbackURL'       : 'http://localhost:3000/auth/twitter/callback'
+        'callbackURL'       : 'http://http://trych-env.us-east-1.elasticbeanstalk.com/auth/twitter/callback'
     },
 
     'googleAuth' : {
         'clientID'      : '668415300177-24gqu1umtos6a5o19f7ve4kn9fri00bl.apps.googleusercontent.com',
         'clientSecret'  : '0IgohWAvHMWSw1_1aCqqM6Nk',
-        'callbackURL'   : 'http://localhost:3000/auth/google/callback'
+        'callbackURL'   : 'http://http://trych-env.us-east-1.elasticbeanstalk.com/auth/google/callback'
     }
 
 };
