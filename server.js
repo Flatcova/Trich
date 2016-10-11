bc194ae7a40164e1a40e27fb525e522a5f1f11f6
Flatcova <m.covar20@gmail.com>
2016-10-11 01:38:54 -0500

    fix

--- server.js
+++ server.js
@@ -1,42 +1,112 @@
-// server.js
-
-// set up ======================================================================
-// get all the tools we need
-var express  = require('express');
-var app      = express();
-var port     = process.env.PORT || 3000;
-var mongoose = require('mongoose');
-var passport = require('passport');
-var flash    = require('connect-flash');
-
-var morgan       = require('morgan');
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
 var cookieParser = require('cookie-parser');
-var bodyParser   = require('body-parser');
-var session      = require('express-session');
+var bodyParser = require('body-parser');
+var morgan = require('morgan');
+var mongoose = require('mongoose');
+var session = require('express-session');
+var flash = require('express-flash');
+var MongoStore = require('connect-mongo')(session);
+var passport = require('passport');
+
+// REQUEST MODELS
+
+var User = require('./models/user');
+
+// SETTING ROUTES
+
+var main = require('./routes/main/index');
+// var category = require('./routes/main/category');
+// var product = require('./routes/main/product');
+// var cart = require('./routes/main/cart');
+
+var signup = require('./routes/users/signup');
+var login = require('./routes/users/login');
+var logout = require('./routes/users/logout');
+// var whishlist = require('./routes/users/whishlist');
+// var history = require('./routes/users/history');
 
 var configDB = require('./config/database.js');
 
+var app = express();
+var engine = require('ejs-mate');
+
 // configuration ===============================================================
-mongoose.connect(configDB.url); // connect to our database
+mongoose.connect(configDB.url, function(err){
+  if (!err)
+    console.log('Connected to database');
+}); // connect to our database
 
-require('./config/passport')(passport); // pass passport for configuration
 
-// set up our express application
-app.use(morgan('dev')); // log every request to the console
-app.use(cookieParser()); // read cookies (needed for auth)
-app.use(bodyParser()); // get information from html forms
 
-app.set('view engine', 'ejs'); // set up ejs for templating
+app.engine('ejs', engine);
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
-// required for passport
-app.use(session({ secret: 'ilovescotchscotchyscotchscotch' })); // session secret
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(session({
+  resave: true,
+  saveUnitialized: true,
+  secret: "T^!c4",
+  store: new MongoStore({url: configDB.url, autoReconnect:true})
+}));
+app.use(flash());
 app.use(passport.initialize());
-app.use(passport.session()); // persistent login sessions
-app.use(flash()); // use connect-flash for flash messages stored in session
+app.use(passport.session());
+app.use(function(req, res, next){
+  res.locals.user = req.user;
+  next();
+});
 
-// routes ======================================================================
-require('./app/routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
 
-// launch ======================================================================
-app.listen(port);
-console.log('The magic happens on port ' + port);
+app.use(express.static(path.join(__dirname, 'public')));
+
+//Middleware
+app.use(morgan('dev'));
+
+app.use('/', main);
+app.use('/signup', signup);
+app.use('/login', login);
+app.use('/logout', logout);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('main/error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('main/error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+
+module.exports = app;
